049dcacf012936d41d83c18566cb097da89f2def
Thanh Nguyen <tienthanh8490@gmail.com>
2015-10-26 16:35:20 +0800

    ESS-227-datachannel #comment Add handlers

--- source/datachannel.js
+++ source/datachannel.js
@@ -1,32 +1,49 @@
 var DataChannel = function(channel){
-	'use strict';
+
 	var self = this;
-	var id = channel.label;
-	var reliable = false;
-	var readyState = 'connecting';
-	var channelType = 'generic';
-	var objectRef = channel;
+
+	self.label = channel.label;
+
+	self.readyState = 'constructed';
+
+	self._channelType = 'generic';
+
+	self._objectRef = channel;
 
 	Event.mixin(this);
 
-	objectRef.onopen = function(event){
-		this._trigger('connected', event);
-	};
+	self._bindHandlers();
 
-	objectRef.onmessage = function(event){
-		this._trigger('message', event);
-	};
-
-	objectRef.onclose = function(event){
-		this._trigger('disconnected', event);
-	};
-
-	objectRef.onerror = function(event){
-		this._trigger('error', event);
-	};
 };
 
 DataChannel.prototype.disconnect = function(){
 	var self = this;
 	objectRef.close();
 };
+
+DataChannel._bindHandlers = function(){
+	var self = this;
+
+	// The native RTCDataChannel is actually opening
+	if (self._objectRef.readyState === 'open'){
+		self._objectRef.onopen = function(event){
+			this._trigger('connected', event);
+		};
+
+		self._objectRef.onmessage = function(event){
+			this._trigger('message', event);
+		};
+
+		self._objectRef.onclose = function(event){
+			this._trigger('disconnected', event);
+		};
+
+		self._objectRef.onerror = function(event){
+			this._trigger('error', event);
+		};
+	}
+	else{
+		throw 'The WebRTC DataChannel is not open. Can not bind handlers';
+	}
+
+}
