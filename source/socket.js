37ce077c815bc447757caa3f955150b78032f69e
Thanh Nguyen <tienthanh8490@gmail.com>
2015-11-16 16:32:03 +0800

    ESS-230-room #comment Singleton socket in case multi room

--- source/socket.js
+++ source/socket.js
@@ -1,69 +1,81 @@
-var Socket = function (options) {
+var Socket = (function (options) {
 
   'use strict';
 
-  var self = this;
+  var self = null;
 
-  if (!options){
-    options = {};
-  }
+  var returnSingleton = function(){
+    if (self){
+      return self;
+    }
 
-  self._signalingServer = options.server || 'signaling.temasys.com.sg';
+    self = this;
 
-  self._socketPorts = {
-    'http': options.httpPorts || [80, 3000],
-    'https': options.httpsPorts || [443, 3443]
+    if (!options){
+      options = {};
+    }
+
+    self._signalingServer = options.server || 'signaling.temasys.com.sg';
+
+    self._socketPorts = {
+      'http': options.httpPorts || [80, 3000],
+      'https': options.httpsPorts || [443, 3443]
+    };
+
+    self._type = options.type || 'WebSocket'; // Default
+
+    self._socketTimeout = options.socketTimeout || 20000;
+
+    self._isSecure = options.secure || false;
+
+    self._readyState = 'constructed';
+
+    self._isXDR = false;
+
+    self._signalingServerProtocol = window.location.protocol.substring(0,window.location.protocol.length-1);
+
+    self._socketMessageTimeout = null;
+
+    self._socketMessageQueue = [];
+
+    self.SOCKET_ERROR = {
+      CONNECTION_FAILED: 0,
+      RECONNECTION_FAILED: -1,
+      CONNECTION_ABORTED: -2,
+      RECONNECTION_ABORTED: -3,
+      RECONNECTION_ATTEMPT: -4
+    };
+
+    self.SOCKET_FALLBACK = {
+      NON_FALLBACK: 'nonfallback',
+      FALLBACK_PORT: 'fallbackPortNonSSL',
+      FALLBACK_SSL_PORT: 'fallbackPortSSL',
+      LONG_POLLING: 'fallbackLongPollingNonSSL',
+      LONG_POLLING_SSL: 'fallbackLongPollingSSL'
+    };
+
+    self._currentSignalingServerPort = null;
+
+    self._channelOpen = false;
+
+    self._objectRef = null; // The native socket.io client object
+
+    self._firstConnect = false;
+
+    self._socketOptions = {
+      reconnection: true,
+      reconnectionAttempts: 2,
+      forceNew: true
+    };
+
+    // Append events settings in here
+    SkylinkEvent._mixin(self);
+
   };
 
-  self._type = options.type || 'WebSocket'; // Default
+  return returnSingleton;
 
-  self._socketTimeout = options.socketTimeout || 20000;
-
-  self._isSecure = options.secure || false;
-
-  self._readyState = 'constructed';
-
-  self._isXDR = false;
-
-  self._signalingServerProtocol = window.location.protocol.substring(0,window.location.protocol.length-1);
-
-  self._socketMessageTimeout = null;
-
-  self._socketMessageQueue = [];
-
-  self.SOCKET_ERROR = {
-    CONNECTION_FAILED: 0,
-    RECONNECTION_FAILED: -1,
-    CONNECTION_ABORTED: -2,
-    RECONNECTION_ABORTED: -3,
-    RECONNECTION_ATTEMPT: -4
-  };
-
-  self.SOCKET_FALLBACK = {
-    NON_FALLBACK: 'nonfallback',
-    FALLBACK_PORT: 'fallbackPortNonSSL',
-    FALLBACK_SSL_PORT: 'fallbackPortSSL',
-    LONG_POLLING: 'fallbackLongPollingNonSSL',
-    LONG_POLLING_SSL: 'fallbackLongPollingSSL'
-  };
-
-  self._currentSignalingServerPort = null;
-
-  self._channelOpen = false;
-
-  self._objectRef = null; // The native socket.io client object
-
-  self._firstConnect = false;
-
-  self._socketOptions = {
-    reconnection: true,
-    reconnectionAttempts: 2,
-    forceNew: true
-  };
-
-  // Append events settings in here
-  SkylinkEvent._mixin(self);
-};
+})();
 
 Socket.prototype._assignPort = function(){
 
