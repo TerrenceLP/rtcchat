0ccb2d478c07e6482b983f8167fbe9c06b9e8d96
TerrenceLP <terrence@porretto.com>
2016-02-08 20:33:21 -0500

    Signed-off-by: TerrenceLP <terrence@porretto.com>

--- tests/config-example.js
+++ tests/config-example.js
@@ -1,5 +1,5 @@
 window.io = require('../node_modules/socket.io-client/socket.io.js');
 window.AdapterJS = require('../node_modules/adapterjs/publish/adapter.screenshare.js');
 window.Skylink = require('../publish/skylink.debug.js').Skylink;
-window.apikey = 'd223305b-ccb4-46cf-8191-180331d8019f';
-window.apisecret = 'pg2aqn3l8rbgw';
+window.apikey = '<% YOUR APP KEY HERE %>';
+window.apisecret = '<% YOUR APP KEY SECRET HERE %>';
